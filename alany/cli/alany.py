a17e785169f842b3a7d8775b8f246bd2b22a95ec
Vlad Pavlyuchkov <pavlyuchkov11@bk.ru>
2022-05-13 10:47:32 +0300

    alany 0.1.0 pre final

--- alany/cli/alany.py
+++ alany/cli/alany.py
@@ -3,6 +3,10 @@ from .. import compiler
 from .main import cli
 
 @cli.command()
-@click.argument('file')
-def run(file):
-    compiler.Compiler().run_file(file)
+@click.argument('file', default='')
+@click.option('--code', '-c', default='')
+def run(file, code):
+    if not file == '':
+        compiler.Compiler().run_file(file)
+    elif not code == '':
+        compiler.Compiler().run(code=code, file='/')
