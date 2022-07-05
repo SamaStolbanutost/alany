fab6ffa72e759d280d7352ee32551dcdb1becef6
Vlad Pavlyuchkov <pavlyuchkov11@bk.ru>
2022-07-05 13:25:51 +0300

    imports fix

--- alany/__main__.py
+++ alany/__main__.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 import sys
-from .cli.alany import run as rn  # noqa
-from .cli.esobyte import esobyte, run  # noqa
-from .cli.pia import install, update, remove # noqa
-from .cli.main import cli
+from alany.cli.alany import run as rn  # noqa
+from alany.cli.esobyte import esobyte, run  # noqa
+from alany.cli.pia import install, update, remove # noqa
+from alany.cli.main import cli
 
 
 def main():
     args = sys.argv
     if '--help' in args or len(args) == 1:
-        print('Alany 0.0.8')
+        print('Alany 0.1.2')
         print('')
     cli()
 
