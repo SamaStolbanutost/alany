85a77b6df7b95fea8cd62b4f81cd8f2368217089
Vlad Pavlyuchkov <pavlyuchkov11@bk.ru>
2022-07-05 13:14:54 +0300

    pip install fix

--- alany/__main__.py
+++ alany/__main__.py
@@ -5,8 +5,14 @@ from .cli.esobyte import esobyte, run  # noqa
 from .cli.pia import install, update, remove # noqa
 from .cli.main import cli
 
-args = sys.argv
-if '--help' in args or len(args) == 1:
-    print('Alany 0.0.8')
-    print('')
-cli()
+
+def main():
+    args = sys.argv
+    if '--help' in args or len(args) == 1:
+        print('Alany 0.0.8')
+        print('')
+    cli()
+
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='alany',
-    version='0.1.2',
-    description='''Programming language for creating bots in AnonyGram
-                   and other programs.''',
-    url='https://github.com/anony-oss/alany',
-    install_requires=[],
-    packages=['alany'],
-    entry_points={'console_scripts': ['alany = alany.core:__main__']}
-)
+setup()
